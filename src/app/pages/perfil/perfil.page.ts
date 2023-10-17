9a42f4b0509ab08a312cf0ccdfc0ae0cb5cdbf24
krlangas08 <carl.rojasr@duocuc.cl>
2023-10-16 23:02:30 -0500

    Diseño y Pagina de perfil

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { StorageService } from '../../service/storage.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage implements OnInit {
+  usuario:any;
 
   constructor( 
-    private route: ActivatedRoute
+    private storage:StorageService,
+              private auth:AngularFireAuth
   ) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-     const id = params.get('id'); 
-  });
+  this.cargarinfoUser();
 
+  }
+  async cargarinfoUser(){
+    const user = await this.storage.obtenerUsuario();
+    const userFirebaseEmail = await this.auth.currentUser;
+    const userFilter = user.filter(e => e.correo == userFirebaseEmail?.email);
+    await userFilter[0].nombre;
+console.log("infoUser",userFilter[0].nombre);
+
+    console.log("property", this.storage.userCorreo);
+    this.usuario = (await this.storage.obtenerUsuario()).filter(e => e.correo == userFirebaseEmail?.email);
+    console.log("USUARIO FILTRADO",this.usuario);
+    
   }
 }
