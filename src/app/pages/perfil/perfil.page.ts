2c1fcedb5a5fedacbaa9f06e120f7a50d6518b2c
krlangas08 <carl.rojasr@duocuc.cl>
2023-10-18 23:37:12 -0500

    cambios de carlitos

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+<<<<<<< HEAD
 import { StorageService } from '../../service/storage.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+=======
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { StorageService } from 'src/app/service/storage.service';
+
+>>>>>>> main
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage implements OnInit {
+<<<<<<< HEAD
   usuario:any;
   isModalOpen = false;
 
@@ -35,3 +42,25 @@ console.log("infoUser",userFilter[0].nombre);
     
   }
 }
+=======
+  usuario : any;
+
+  constructor(private storage:StorageService,
+              private auth:AngularFireAuth) { }
+
+  ngOnInit() {
+    this.cargarInfoUsuario();
+  }
+  async cargarInfoUsuario(){
+    var userCorreo=await this.auth.currentUser;
+    console.log("Correo de usuario",userCorreo?.email)
+    
+    this.usuario = (await this.storage.obtenerUsuario()).filter(
+      (usuario)=>usuario.correo == userCorreo?.email
+      );
+    
+
+  }
+
+}
+>>>>>>> main
